f7b6c8e401c6543a26bbff85aae28e80f927d520
mclaudiany <mp.claudiany@gmail.com>
2025-10-11 15:22:39 -0300

    atividade de frontend - 16.09.2025

---
+++ aula16.09/script.js
@@ -0,0 +1,26 @@
+const inputTexto = document.getElementById("texto");
+const button = document.getElementById("btn");
+
+button.onclick = () => {
+  //alert(`Eu digitei: ${inputTexto.value}`);
+  alert('Eu digitei ahhaha:' + inputTexto.value);
+};
+
+
+
+
+
+
+
+let nome = "ana maria";
+let idade =  20;
+let possuiCarro = true;
+
+let pessoa = {
+    "nome": "ana mnaria",
+    "idade": 20,
+    "possuiCarro": true
+}
+
+let listaFrutas = ["melancia", "banana", "maçã"]
+
